da80da154fe8134b34497b3a42d4718378d8be60
Ralph <ralphoburu@gmail.com>
2021-03-05 16:39:21 +0300

    Add assosiation of users and events

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_04_122605) do
+ActiveRecord::Schema.define(version: 2021_03_05_101729) do
+
+  create_table "events", force: :cascade do |t|
+    t.date "date"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
