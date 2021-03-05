b5145a2447ef5034a3699b9e4e7b8aa5558fa687
Ralph <ralphoburu@gmail.com>
2021-03-05 11:53:47 +0300

    Add the sessions controler and actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,15 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create 
+    user = User.find_by(params[:name])
+    if user 
+      session[:user_id] = user
+      redirect_to root_path, notice: "Welcome #{user.id}!"
+    else
+      flash.now.alert = 'Login faild'
+      render :new
+    end
+  end
 end
