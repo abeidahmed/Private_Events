c69e2ff25524909f4a31227f6ce13e33fd76b652
Ralph <ralphoburu@gmail.com>
2021-03-08 12:40:46 +0300

    Add attendance routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :sessions, only: %i[new create destroy]
   resources :users, only: %i[new create show], dependant: :destroy
   resources :events, only: %i[index new create show]
+  post 'attendance', to: 'events#create_attendance'
   # delete 'sessions#destroy'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
